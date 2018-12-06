7f84990d2733f2f6da33c4f64761dee2bc05f9e1
caudate_julie <kyra-mensk@yandex.ru>
2018-12-06 17:31:26 +0300

    01B findChar doesn't get value for answer

--- 01.hs
+++ 01.hs
@@ -2,30 +2,28 @@
 
 my_read :: [Char] -> Int
 my_read s = 
-
     if s!!0 == '+'
     then my_read (tail s)
     else read s
 
 
-findChar :: (Eq a) => a -> [a] -> Int -> Int
-findChar c [] n = n
-findChar c (x:xs) n = if (x == c) 
-    then n 
-    else findChar c xs (n+1)
+findChar :: (Eq a) => a -> [a] -> Int
+findChar c [] = 0
+findChar c (x:xs) = if (x == c) 
+    then 0
+    else (findChar c xs) + 1
 
 
 split :: (Eq a) => a -> [a] -> [[a]]
 split c [] = []
 split c s = 
-    let n = findChar c s 0 in
+    let n = findChar c s in
     [take n s] ++ split c (drop (n+1) s)
 
 
 my_map :: (a -> b) -> [a] -> [b]
 my_map f [] = []
 my_map f (x:xs) = [f x] ++ my_map f xs   -- ++ for concatenate
-
 
 ---------------------------------------------------------
 
@@ -34,11 +32,8 @@ reduce f def [] = def
 reduce f def (x:xs) = f x (reduce f def xs)
 
 
-op_plus a b = a + b
-
-
 solve_A :: [Char] -> Int
-solve_A s = reduce op_plus 0 (my_map my_read (split '\n' s))
+solve_A s = reduce (+) 0 (my_map my_read (split '\n' s))
 
 
 ---------------------------------------------------------
@@ -55,7 +50,7 @@ solve_B s = duplicates (my_map my_read (split '\n' s)) [0]
 main = do
     s <- readFile "01_input.txt"
     -- let s = "+3\n+3\n+4\n-2\n-4\n"
-    putStrLn (show (solve_B s))               -- show ~ repr
+    putStrLn (show (solve_A s))               -- show ~ repr
 
 
 
