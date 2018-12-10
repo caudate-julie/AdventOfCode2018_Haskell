fc55d1ba6ddcd6e84ab3adb5328ca6006361471a
caudate_julie <kyra-mensk@yandex.ru>
2018-12-10 17:19:00 +0300

    03B working version

--- 03.hs
+++ 03.hs
@@ -13,20 +13,21 @@ slice x y s =
 
 
 -- "#4 @ 342,752: 19x17" => (342, 752, 19, 17)
-parse_claim :: [Char] -> (Int, Int, Int, Int)
+parse_claim :: [Char] -> (Int, Int, Int, Int, Int)
 parse_claim s = 
-    ( read (slice '@' ',' s),
+    ( read (slice '#' '@' s),
+      read (slice '@' ',' s),
       read (slice ',' ':' s),
       read (slice ':' 'x' s),
       read (slice 'x' ' ' s) )
 
 
-parse_claims :: [[Char]] -> [(Int, Int, Int, Int)]
+parse_claims :: [[Char]] -> [(Int, Int, Int, Int, Int)]
 parse_claims lines = map parse_claim lines
 
 
-claims_to_rectangles :: [(Int, Int, Int, Int)] -> [(Int, Int, Int, Int)]
-claims_to_rectangles xs = [(x, x+a, y, y+b) | (x, y, a, b) <- xs]
+claim_to_rectangle :: (Int, Int, Int, Int, Int) -> (Int, Int, Int, Int)
+claim_to_rectangle (_, x, y, a, b) = (x, x+a, y, y+b)
 
 
 --------------------------------------------------------------
@@ -35,7 +36,7 @@ pixel_in_area :: (Int, Int) -> (Int, Int, Int, Int) -> Bool
 pixel_in_area (x, y) (x1, x2, y1, y2) = (x1 <= x) && (x < x2) && (y1 <= y) && (y < y2)
 
 
--- rectangle is in area if it overlaps
+-- area -> rectangle in question
 rectangle_in_area :: (Int, Int, Int, Int) -> (Int, Int, Int, Int) -> Bool
 rectangle_in_area (ax1, ax2, ay1, ay2) (x1, x2, y1, y2) =
     (ax1 < x2) && (ax2 > x1) && 
@@ -48,7 +49,7 @@ pixel_overlap p as = case filter (pixel_in_area p) as of
     _      -> False
 
 
-count_area :: (Int, Int, Int, Int) -> [(Int, Int, Int, Int)]  -> Int
+count_area :: (Int, Int, Int, Int) -> [(Int, Int, Int, Int)] -> Int
 count_area a rs = 
     let rectangles = [r | r <- rs, rectangle_in_area a r]
         (x1, x2, y1, y2) = a
@@ -58,15 +59,26 @@ count_area a rs =
 
 solve_A :: [[Char]] -> Int
 solve_A s = 
-    let areas = [(i*32, i*32 + 32, j*32, j*32 + 32) | i <- [0..31], j <- [0..31]] 
-        rectangles = (claims_to_rectangles . parse_claims) s in
+    -- let areas = [(i*32, i*32 + 32, j*32, j*32 + 32) | i <- [0..31], j <- [0..31]] 
+    let areas = [(0, 1000, 0, 1000)]
+        rectangles = (map claim_to_rectangle (parse_claims s)) in
     sum [count_area a rectangles | a <- areas]
 
 ------------------------------------------------------
 
+-- rectangle has no overlaps
+overlapping ::  (Int, Int, Int, Int) -> [(Int, Int, Int, Int)] -> Bool
+overlapping r rs = any (rectangle_in_area r) rs
 
-solve_B :: [[Char]] -> [Char]
-solve_B s = error "TODO"
+find_not_overlapping :: [(Int, Int, Int, Int, Int)] -> [Int]
+find_not_overlapping s = 
+    let rectangles = map claim_to_rectangle s
+        z = zip [id | (id, _, _, _, _) <- s] rectangles in
+    [id | (id, r) <- z, not (overlapping r ((take (id-1) rectangles) ++ (drop id rectangles)))]
+
+solve_B :: [[Char]] -> [Int]
+solve_B s =  find_not_overlapping (parse_claims s)
+
 
 -----------------------------------------------------
 
@@ -75,8 +87,7 @@ solve_B s = error "TODO"
 main = do
     s <- readFile "03_input.txt"
     let lines = split '\n' s
-    -- let lines = ["#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"]
-    -- let s = (claims_to_rectangles . parse_claims) lines
     -- putStrLn (show (count_pixel (3, 2) s))
-    putStrLn (show (solve_A lines))
+    -- putStrLn (show (solve_A lines))
+    putStrLn (show (solve_B lines))
 
